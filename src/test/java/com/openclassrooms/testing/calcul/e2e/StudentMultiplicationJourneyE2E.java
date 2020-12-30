7dfbaec2d6908018c6093a1fb7cc61227faf1784
Papoune <npapon@live.fr>
2020-12-30 12:43:12 +0100

    last commit

--- src/test/java/com/openclassrooms/testing/calcul/e2e/StudentMultiplicationJourneyE2E.java
+++ src/test/java/com/openclassrooms/testing/calcul/e2e/StudentMultiplicationJourneyE2E.java
@@ -1,11 +1,18 @@
 package com.openclassrooms.testing.calcul.e2e;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -37,6 +44,30 @@ public class StudentMultiplicationJourneyE2E {
         if ( webDriver != null ) {
             webDriver.quit();
         }
+    }
+
+    @Test
+    public void lutilisateurUtiliseLeCalculateurPourMultiplier2par16() {
+        // GIVEN
+        webDriver.get( baseUrl );
+        WebElement leftField = webDriver.findElement( By.id( "left" ) );
+        WebElement rightField = webDriver.findElement( By.id( "right" ) );
+        WebElement typeDropButton = webDriver.findElement( By.id( "type" ) );
+        WebElement submitButton = webDriver.findElement( By.id( "submit" ) );
+
+        // WHEN
+        leftField.sendKeys( "2" );
+        rightField.sendKeys( "16" );
+        typeDropButton.sendKeys( "x" );
+        submitButton.click();
+
+        // THEN
+
+        final WebDriverWait attendUneReponse = new WebDriverWait( webDriver, 5 );
+        WebElement solutionElement = attendUneReponse.until( ExpectedConditions.presenceOfElementLocated( By.id( "solution" ) ) );
+        String solution = solutionElement.getText();
+        assertThat( solution ).isEqualTo( "32" );
+
     }
 
 }
